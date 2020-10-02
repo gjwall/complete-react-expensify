edbc9faf17933e1dadccebe172d6c02065087d7a
Graham Wallace <grahamjwallace@gmail.com>
2020-10-02 18:18:23 +0100

    After sorting redux data, end of section 10

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -134,6 +134,12 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a,b) => {
+        if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        } else {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }
     });
 };
 
@@ -151,17 +157,17 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
- const expenseOne = store.dispatch(AddExpense( {description: 'Rent', amount: 100, createdAt: 1000 }));
+ const expenseOne = store.dispatch(AddExpense( {description: 'Rent', amount: 100, createdAt: -11000 }));
  const expenseTwo = store.dispatch(AddExpense( {description: 'Coffee', amount: 300, createdAt: -1000 }));
 // store.dispatch(removeExpense( {id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense( expenseTwo.expense.id, {amount: 500} ));
 
-store.dispatch(setTextFilter('re'));
+// store.dispatch(setTextFilter('re'));
 // store.dispatch(setTextFilter(''));
 
-// store.dispatch(sortByAmount());
-// store.dispatch(sortByDate());   
+//store.dispatch(sortByAmount());
+store.dispatch(sortByDate());   
 
 // store.dispatch(setStartDate(0));
 // // store.dispatch(setStartDate());
