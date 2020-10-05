792d54b46255fae353978c70ea8084d1c20a91ea
Graham Wallace <grahamjwallace@gmail.com>
2020-10-05 20:03:11 +0100

    After connecting store and component with react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
@@ -21,4 +22,16 @@ store.dispatch(addExpense( { description: 'gas bill' } ));
 store.dispatch(setTextFilter('bill'));
 store.dispatch(setTextFilter('water'));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app')); 
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000)
+
+// This sets up a provider that passes a store into all of our components
+const jsx = (
+    <Provider store={store}>
+        <AppRouter /> 
+    </Provider>
+);
+
+//ReactDOM.render(<AppRouter />, document.getElementById('app')); 
+ReactDOM.render(jsx, document.getElementById('app')); 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,12 @@ import {createStore, combineReducers} from 'redux';
 import expensesReducer from './../reducers/expenses';
 import filterReducer from './../reducers/filters';
 
+// https://github.com/zalmoxisus/redux-devtools-extension
+// const store = createStore(
+//     reducer, /* preloadedState, */
+//  +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
 export default () => {
     const store = createStore(
         combineReducers({
