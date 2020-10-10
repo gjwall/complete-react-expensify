27ec40c612e280555dd5cb67412ba1a273a0f188
Graham Wallace <grahamjwallace@gmail.com>
2020-10-10 09:28:15 +0100

    After setting up jest

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,7 +1,18 @@
-// Initial test file
+// npm run test -- --watch
 const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
     expect(result).toBe(7);
 });
+
+test('Check that the greeting is as expected', () => {
+    const result = generateGreeting('Bob');
+    expect(result).toBe(`Hello Bob!`);
+}); 
+
+test('Check that the greeting for no name', () => {
+    const result = generateGreeting();
+    expect(result).toBe(`Hello Anonymous!`);
+}); 
