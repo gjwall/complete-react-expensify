ebf75f6aee10e5ce421b6266198ea1edbdfa922b
Graham Wallace <grahamjwallace@gmail.com>
2020-10-14 16:08:17 +0100

    After Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,8 @@
-import { render } from 'enzyme';
-import { TestScheduler } from 'jest';
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 import Header from './../../components/Header';
 
-test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+ test('should render Header correctly', () => {
+     const wrapper = shallow(<Header />);
+     expect(wrapper).toMatchSnapshot(); 
 });
