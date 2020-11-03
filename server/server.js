6261d6af45f063d9a55bf76a1924b1977896ba0b
Graham Wallace <grahamjwallace@gmail.com>
2020-11-03 12:45:38 +0000

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
 
