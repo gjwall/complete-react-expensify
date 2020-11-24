1c42440fca179907efe92178126641d257153eab
Graham Wallace <grahamjwallace@gmail.com>
2020-11-24 14:35:11 +0000

    After logging out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from './../actions/auth';
 
-const Header = () => (
-    <div>
+export const Header = ( { startLogout} ) => (
+    <header>
         <h1>Expensify</h1>
         <NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create expense</NavLink>
-        
-    </div>
+        <button onClick={startLogout}>Logout</button>
+    </header>
 );
+const mapDispatchToProps = (dispatch) => ({
+        startLogout: () => dispatch(startLogout())
+});
 
-export default Header;
+export default connect(undefined, mapDispatchToProps)(Header);
