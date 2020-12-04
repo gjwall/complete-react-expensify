e3c9f88413b3d91f88d1091d7ca09e3260f6b295
Graham Wallace <grahamjwallace@gmail.com>
2020-12-04 22:09:59 +0000

    After private only routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import { createBrowserHistory } from 'history';
 import AddExpensePage from './../components/AddExpensePage';
 import EditExpensePage from './../components/EditExpensePage';
 import ExpenseDashboardPage from './../components/ExpenseDashboardPage';
-import Header from './../components/Header';
 import HelpPage from './../components/HelpPage';
 import LoginPage from './../components/LoginPage';
 import NotFoundPage from './../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 // To avoid history not working, install the same version that react-router installed
 // see https://stackoverflow.com/questions/40483942/react-router-cannot-resolve-module-history-missing-lib-folder
@@ -16,13 +16,11 @@ export const history = createBrowserHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
-            <h1>Header</h1>
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/edit/:id" component={EditExpensePage} /> 
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} /> 
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
