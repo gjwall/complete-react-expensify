ee996024008ca5be4d9b095cb62364047731d33e
Graham Wallace <grahamjwallace@gmail.com>
2020-12-07 15:56:40 +0000

    After styling buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from './../actions/auth';
 
 export const Header = ( { startLogout } ) => (
-    <header>
-        <h1>Expensify</h1>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/create">Create expense</Link>
-        <button onClick={startLogout}>Logout</button>
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Expensify</h1>
+                </Link>
+                <button class="button" onClick={startLogout}>Logout</button>
+            </div>
+        </div>
     </header>
 );
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,9 +6,9 @@ export const LoginPage = ( { startLogin }) => {
     return (
         <div className="box-layout">
             <div className="box-layout__box">
-                <h1 className="box-layout__title">Expensify App</h1>
+                <h1 className="box-layout__title">Expensify</h1>
                 <p>Get expenses under control</p>
-                <button onClick={startLogin}>Login</button>
+                <button className="button" onClick={startLogin}>Login with Google</button>
             </div>
         </div>
     );
